50b318edc9de943137262d7d68675ab812b34c40
mateuszmaniecki <40563786+mateuszmaniecki@users.noreply.github.com>
2018-06-25 12:12:04 +0200

    Update update.php

--- lang/Polish/modules/update.php
+++ lang/Polish/modules/update.php
@@ -28,14 +28,14 @@ define('OGP_LANG_no_access', "Potrzebujesz uprawnień administratora, aby uzyska
 define('OGP_LANG_dwl_update', "Pobieranie Aktualizacji...");
 define('OGP_LANG_dwl_complete', "Pobieranie zakończone.");
 define('OGP_LANG_install_update', "Instalowanie aktualizacji...");
-define('OGP_LANG_update_complete', "Aktualizacja zakonczona.");
-define('OGP_LANG_ignored_files', "%s zignorowane pliki.");
-define('OGP_LANG_not_updated_files_blacklisted', "Not updated/installed files (blacklisted):<br>%s");
+define('OGP_LANG_update_complete', "Aktualizacja zakończona.");
+define('OGP_LANG_ignored_files', "%s zignorowanych pliki.");
+define('OGP_LANG_not_updated_files_blacklisted', "Nie zaktualizowano plików (czarna lista):<br>%s");
 define('OGP_LANG_latest_version', "Najnowsza wersja");
 define('OGP_LANG_panel_version', "Wersja panelu");
 define('OGP_LANG_update_now', "Aktualizuj Teraz");
-define('OGP_LANG_the_panel_is_up_to_date', "Panel jest Aktualny.");
-define('OGP_LANG_files_overwritten', "%s nadpisane pliki");
+define('OGP_LANG_the_panel_is_up_to_date', "Posiadasz aktualną wersję panelu.");
+define('OGP_LANG_files_overwritten', "%s nadpisanych pliki");
 define('OGP_LANG_files_not_overwritten', "%s files are NOT overwritten due to blacklist");
 define('OGP_LANG_can_not_update_non_writable_files', "Nie można zaktualizować, ponieważ następujące pliki/foldery nie mają praw do zapisu.");
 define('OGP_LANG_dwl_failed', "Adres pobierania nie jest dostepny: \"%s\".<br> Spróbuj ponownie później.");
@@ -53,7 +53,7 @@ define('OGP_LANG_blacklist_files_info', "- wszystkie wybrane pliki nie zostaną 
 define('OGP_LANG_save_to_blacklist', "Zapisz na czarnej liście");
 define('OGP_LANG_no_new_updates', "Brak nowych aktualizacji");
 define('OGP_LANG_module_file_missing', "directory is missing the module.php file.");
-define('OGP_LANG_query_failed', "Failed to execute query");
+define('OGP_LANG_query_failed', "Błąd wykonania zadania");
 define('OGP_LANG_query_failed_2', "to database.");
 define('OGP_LANG_missing_zip_extension', "The php-zip extension is not loaded. Please, enable it to use the Update module.");
 ?>
